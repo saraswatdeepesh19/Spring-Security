2f032b80e5c2f3f5a362ac036b1935d5a56716a0
Deepesh <saraswatdeepesh19@gmail.com>
2022-12-24 17:09:32 +0530

    Spring Security Learning

---
+++ springsecuritybasic/src/main/java/com/deepesh/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.deepesh.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+
+	@GetMapping("/welcome")
+	public String sayWelcome() {
+		return "Welcome to Spring Application with out Security";
+	}
+
+}
