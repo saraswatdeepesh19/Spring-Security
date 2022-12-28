0dd03b84c5bd34261b11cb275d73ffd159e0ae10
Deepesh <saraswatdeepesh19@gmail.com>
2022-12-28 18:12:22 +0530

    Section 3: multiple User Configuration

---
+++ springsecurity-Section3/src/main/java/com/deepesh/bank/config/ProjectSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.deepesh.bank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		//http.authorizeHttpRequests().anyRequest().authenticated(); // To authenticate All the Requests
+		http.authorizeHttpRequests()
+				.requestMatchers("/myAccount","/myLoan","/myCard","/myBalance").authenticated()
+						.requestMatchers("/myContact","/myNotice").permitAll();
+		http.formLogin();
+		http.httpBasic();
+		return (SecurityFilterChain) http.build();
+
+	}
+
+}
---
+++ springsecurity-Section3/src/main/java/com/deepesh/bank/controller/ContactController.java
@@ -0,0 +1,13 @@
+package com.deepesh.bank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+
+	@GetMapping("/myContact")
+	public String getContactDetails() {
+		return "Here are the Contact details from the DB";
+	}
+}
