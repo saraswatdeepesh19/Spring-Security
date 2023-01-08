d15f7b1527883ec62cfb5f4e984506996fe8a854
Deepesh <saraswatdeepesh19@gmail.com>
2023-01-09 02:11:50 +0530

    CSRF AND CORS Changes

--- springsecurity-Section5/src/main/java/com/deepesh/bank/config/ProjectSecurityConfig.java
+++ springsecurity-Section5/src/main/java/com/deepesh/bank/config/ProjectSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.deepesh.bank.config;
 
+import com.deepesh.bank.filter.CsrfCookieFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +9,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -14,8 +22,21 @@ public class ProjectSecurityConfig {
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 		//http.authorizeHttpRequests().anyRequest().authenticated(); // To authenticate All the Requests
-		http
-				.csrf().disable()
+		http.cors().configurationSource(new CorsConfigurationSource() {
+					@Override
+					public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+						CorsConfiguration configuration = new CorsConfiguration();
+						configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+						configuration.setAllowCredentials(true);
+						configuration.setAllowedMethods(Collections.singletonList("*"));
+						configuration.setAllowedHeaders(Collections.singletonList("*"));
+						configuration.setMaxAge(3600L);
+						return configuration;
+					}
+				}).and()
+				.csrf().ignoringRequestMatchers("/register")
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+				.and().addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
 				.authorizeHttpRequests()
 				.requestMatchers("/myAccount", "/myLoan", "/myCard", "/myBalance")
 				.authenticated().requestMatchers("/myContact", "/myNotice", "/register").permitAll();
